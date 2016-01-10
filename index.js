e861223efcf791daa8ade099980656c31f203938
Alvin Teh <i@alvinteh.me>
2016-01-10 08:44:29 +0000

    General: replaced double quotes with single quotes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "extends": "airbnb",
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "node": true
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'browser': true,
+    'node': true
   },
-  "rules": {
-    "comma-dangle": 0,
-    "brace-style": [2, "stroustrup", { "allowSingleLine": true }],
-    "indent": [2, 2, { "SwitchCase": 1 }]
+  'rules': {
+    'comma-dangle': 0,
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
+    'indent': [2, 2, { 'SwitchCase': 1 }]
   }
 };
